cecca726a77bd77b13c9f73dfe6f7661f169cefe
xKingPanda <71983508+xKingPanda@users.noreply.github.com>
2024-09-13 19:14:15 +0000

    finished lab1

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,20 +1,28 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name:Michael Fuller
+#Date:9/10/2024
+#Assignment:Lab 1
 
 def main():
   print("First Program")
   #Say hello
+  print("Hello!")
   
   #Ask for the user's name
+  name = input("What is your name? ")
 
   #Use the user's name in the program.
+  print(f"Nice to meet you, {name}!")
 
   #Ask the user for their age.
+  age = int(input("How old are you? "))
 
   #Tell the user what year they were born in.
   #Assume that they have not had their birthday yet this year.
+  current_year = 2024
+  birth_year = current_year - age
+
+  print(f"You were born in {birth_year}, {name}.")
 
 
 #Call the main function if this is the file being run.
--- MadLib.py
+++ MadLib.py
@@ -1,13 +1,26 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name:Michael Fuller
+#Date:9/10/2024
+#Assignment:lab1
 
 def main():
   print("Madlib")
   #Ask user for words
+  hero_name = input("Enter the name of your hero: ")
+  monster = input("Enter a type of monster: ")
+  weapon = input("Enter a type of weapon: ")
+  magical_item = input("Enter a magical item: ")
+  place= input("Enter a fantasy place: ")
+  action = input("Enter an action (past tense): ")
+
 
   #Print the story with the user supplied words.
+  print(f"Brave hero {hero_name} ventured into the {place}.")
+  print(f"There, they encountered a terrifying {monster}, angry and ready to attack!")
+  print(f"With their mighty {weapon}, {hero_name} fought fiercely, but the {monster} was too strong.")
+  print(f"Just when all seemed lost, {hero_name} remembered the {magical_item} they carried.")
+  print(f"With one final move, {hero_name} used the {magical_item} and {action} the {monster}, saving the {place} from destruction!")
+
 
 
 
